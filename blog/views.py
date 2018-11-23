687e65094e804cb9ad0a04b44f6ee5bc7ff69f53
钟君杰 <171868430@qq.com>
2018-11-23 15:03:44 +0800

    add post form using django.forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from . import models, forms
 
 def post_list(request):
@@ -15,7 +15,18 @@ def post_detail(request, pk):
 
 
 def post_new(request):
-    form = forms.PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    if request.method == 'GET':
+        form = forms.PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+    else:
+        form = forms.PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+
+
 
 
