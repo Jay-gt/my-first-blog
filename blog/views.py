f646968ec7afbbff29f867cf8ed423301efcfcfc
钟君杰 <171868430@qq.com>
2018-11-23 12:48:16 +0800

    show the detail of each post

--- blog/views.py
+++ blog/views.py
@@ -11,4 +11,5 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(models.Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+    # return HttpResponse('post:'+pk)
 
